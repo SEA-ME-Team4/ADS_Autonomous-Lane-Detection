b9d117ff945e6c50224c47f30159b38862f4af19
ho <ws000617@gmail.com>
2023-12-30 16:58:06 +0100

    Modify comment

--- train/evaluate.py
+++ train/evaluate.py
@@ -14,10 +14,8 @@ def evaluate(net, dataloader, device, amp):
     # iterate over the validation set
     with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
         for batch in tqdm(dataloader, total=num_val_batches, desc='Validation round', unit='batch', leave=False):
-            image, mask_true = batch['image'], batch['mask']
-
-            # move images and labels to correct device and type
-            image = image.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+            image, mask_true, _ = batch  # instance_image는 사용하지 않는다면 무시
+            image = image.to(device=device, dtype=torch.float32)
             mask_true = mask_true.to(device=device, dtype=torch.long)
 
             # predict the mask
